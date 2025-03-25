7a0d0d0e7ac75ea4dc2a9335769485b1c72ff734
ykyam <kyaminuu@icloud.com>
2025-03-25 21:37:16 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>課題007</title>
+</head>
+<body>
+  <?php
+  $vegetables = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+  print_r($vegetables);
+  ?>
+</body>
+</html>
