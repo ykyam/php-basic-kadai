475833811080d0951601ee866fcb8c7b3b3e16f9
ykyam <kyaminuu@icloud.com>
2025-04-06 15:05:43 +0900

    Add files via upload

--- kadai_sort/sort.php
+++ kadai_sort/sort.php
@@ -8,21 +8,21 @@
   <p>
     <?php
     function sort_2way($array, $order) {
-      if ($order === 'asc') {
+      if ($order === 'TRUE') {
         sort($array);
-      } elseif ($order === 'desc') {
+      } elseif ($order === 'FALSE') {
         rsort($array);
       }
       return $array;
     }
     $nums = [15, 4, 18, 23, 10];
     echo "昇順にソートします。<br>";
-    $sorted_nums = sort_2way($nums, 'asc');
+    $sorted_nums = sort_2way($nums, 'TRUE');
     foreach ($sorted_nums as $num) {
       echo $num . "<br>";
     }
     echo "降順にソートします。<br>";
-    $sorted_nums = sort_2way($nums, 'desc');
+    $sorted_nums = sort_2way($nums, 'FALSE');
     foreach ($sorted_nums as $num) {
       echo $num . "<br>";
     }
