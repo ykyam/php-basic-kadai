c590c32bc8ebd64837205efd8597d3b8df52c566
ykyam <kyaminuu@icloud.com>
2025-04-06 16:25:44 +0900

    Add files via upload

---
+++ kadai_sort/kadai_sort/sort.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP基礎編</title>
+</head>
+<body>
+  <p>
+    <?php
+    function sort_2way($array, $order) {
+      if ($order) {
+        sort($array);
+      } else {
+        rsort($array);
+      }
+      return $array;
+    }
+    $nums = [15, 4, 18, 23, 10];
+    echo "昇順にソートします。<br>";
+    $sorted_nums = sort_2way($nums, TRUE);
+    foreach ($sorted_nums as $num) {
+      echo $num . "<br>";
+    }
+    echo "降順にソートします。<br>";
+    $sorted_nums = sort_2way($nums, FALSE);
+    foreach ($sorted_nums as $num) {
+      echo $num . "<br>";
+    }
+    ?>
+  </p>
+</body>
+</html>
